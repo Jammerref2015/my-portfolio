3f67a534e9a243a8cbbdd3635c427c222673b550
Adrian Looney <adrian.looney@gmail.com>
2021-03-17 12:05:12 +0000

    initial home page styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,17 @@
 import React from "react"
+import image from "../temp_bg.jpg"
 
 export default function Home() {
-    return <h1>Home Page</h1>
+    return (
+        <main>
+            <img 
+                src={image}
+                alt="temp_bg"
+                className="absolute object-cover w-full h-full"
+            />
+            <section className="relative flex justify-center min-h-screen lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Adrian Looney</h1>
+            </section>
+        </main>
+    )
 }
